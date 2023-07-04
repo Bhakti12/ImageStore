56fded0947a4ba75101afb874018cdeaaa18cd6b
Bhakti12 <sanghanibhakti922@gmail.com>
2023-07-04 13:03:27 +0530

    added get and post

---
+++ config.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const fbconfig = {
+  use_database: true,
+  host: "localhost",
+  username: "root",
+  database: "facebook",
+};
---
+++ db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+export function DatabaseConnection() {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/imageDemo")
+    .then(() => {
+      console.log("connected sucessfully!!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
